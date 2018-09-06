7880ef363ccc908726ceaaec465ca57c1c7aeb4c
Gavin Ong <gavin.ong@hexainitiative.com>
2018-09-06 14:28:12 +0800

    commit

---
+++ AIAHealthUI/View/AHealthPlus/HealthReward/HealthRewardViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HealthRewardViewController.swift
+//  AIAHealthUI
+//
+//  Created by Gavin.ong on 06/09/2018.
+//  Copyright © 2018 Gavin.ong. All rights reserved.
+//
+
+import UIKit
+
+class HealthRewardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
---
+++ AIAHealthUI/View/Common/View/HealthReward/MainContentHealthRewardView.swift
@@ -0,0 +1,15 @@
+//
+//  MainContentHealthRewardView.swift
+//  AIAHealthUI
+//
+//  Created by Gavin.ong on 06/09/2018.
+//  Copyright © 2018 Gavin.ong. All rights reserved.
+//
+
+import UIKit
+
+class MainContentHealthRewardView: UIView {
+
+ 
+
+}
